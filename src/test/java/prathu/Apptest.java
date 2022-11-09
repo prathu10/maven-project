ee59d86b242babdd8287e54c4d83860ead0a07c9
Prathamesh <prathamesh4402@gmail.com>
2022-11-10 01:42:11 +0530

    first commit

---
+++ src/test/java/prathu/Apptest.java
@@ -0,0 +1,14 @@
+package prathu;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals("Hello Maven",new String("Hello Maven"));
+	}
+
+}
